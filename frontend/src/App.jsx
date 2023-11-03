3bf124aea36e050a23491813c1eabc82dfe83b85
matthewmonolith <matthew.page2016@outlook.com>
2023-11-03 11:17:50 +0000

    minor syntax/code tidying

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Navbar from './components/Navbar'
-import { ChakraProvider } from '@chakra-ui/react'
+import Navbar from './components/Navbar';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
 
