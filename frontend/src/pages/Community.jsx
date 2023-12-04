2f73e78074431860a29fa3c3f9622702be2a7449
matthewmonolith <matthew.page2016@outlook.com>
2023-12-04 12:46:56 +0000

    small changes to community page

--- frontend/src/pages/Community.jsx
+++ frontend/src/pages/Community.jsx
@@ -1,43 +1,59 @@
-import React from 'react';
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 import UserInfo from "../components/UserInfo";
-import { Flex, Box, Text, Divider, Stack, VStack, Heading, Input, Textarea, Button } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  Text,
+  Divider,
+  Stack,
+  VStack,
+  Heading,
+  Input,
+  Textarea,
+  Button,
+} from "@chakra-ui/react";
 import { useNavbarHeight } from "../components/NavbarHeightContext";
-import { useGetPostsQuery, useCreateProductMutation } from '../slices/postsApiSlice';
-import CreatePost from '../components/createPost';
+import {
+  useGetPostsQuery,
+  useCreateProductMutation,
+} from "../slices/postsApiSlice";
+import CreatePost from "../components/createPost";
 
 const Community = () => {
   const { data: posts, isLoading, error, refetch } = useGetPostsQuery();
   const [createPost] = useCreateProductMutation();
   const navbarHeight = useNavbarHeight();
 
-  const [title, setTitle] = useState('');
-  const [caption, setCaption] = useState('');
+  const [title, setTitle] = useState("");
+  const [caption, setCaption] = useState("");
 
   const createPostHandler = async (e) => {
     e.preventDefault();
     try {
-      await createPost({title, caption});
-      setTitle('');
-      setCaption('');
+      await createPost({ title, caption });
+      setTitle("");
+      setCaption("");
       refetch();
     } catch (error) {
-      console.error('Error creating post:', error);
+      console.error("Error creating post:", error);
     }
   };
 
   return (
-    <Flex direction="column" alignItems="center" marginTop={`${navbarHeight}px`}>
+    <Flex
+      direction="column"
+      alignItems="center"
+      marginTop={`${navbarHeight}px`}
+    >
       {isLoading ? (
         <Text>Loading...</Text>
       ) : error ? (
-        <Text color="red.500">
-          {error?.data?.message || error.error}
-        </Text>
+        <Text color="red.500">{error?.data?.message || error.error}</Text>
       ) : (
         <Flex width="100%" justify="center">
-          <UserInfo />
-          <Box p="4" borderWidth="1px" borderRadius="lg" boxShadow="md">
+          {/* <UserInfo /> */}
+          <Box p="4" borderWidth="1px" borderRadius="lg" boxShadow="md" maxHeight="250px">
             <Heading as="h2" mb="4" fontSize="xl">
               Create a New Post
             </Heading>
@@ -63,13 +79,24 @@ const Community = () => {
           <Divider orientation="vertical" mx="4" />
           <VStack align="start" spacing="4">
             {posts.map((post) => (
-              <Box key={post._id} width="400px" p="4" borderWidth="1px" borderRadius="lg" boxShadow="md">
-                <Text fontSize="xl" fontWeight="bold">{post.title}</Text>
+              <Box
+                key={post._id}
+                width="400px"
+                p="4"
+                borderWidth="1px"
+                borderRadius="lg"
+                boxShadow="md"
+              >
+                <Text fontSize="xl" fontWeight="bold">
+                  {post.title}
+                </Text>
                 <Text color="gray.600">{post.caption}</Text>
                 <Divider my="2" />
                 <Stack direction="row" justify="space-between">
                   <Text>Likes: {post.likes}</Text>
-                  <Text color="gray.500">Created At: {new Date(post.createdAt).toLocaleString()}</Text>
+                  <Text color="gray.500">
+                    Created At: {new Date(post.createdAt).toLocaleString()}
+                  </Text>
                 </Stack>
               </Box>
             ))}
