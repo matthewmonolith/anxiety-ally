b40580ca0a2fb25b95ec5c310c049285dca6b901
matthewmonolith <matthew.page2016@outlook.com>
2024-01-16 15:50:08 +0000

    refetch and ui improvements

--- frontend/src/components/UserPosts.jsx
+++ frontend/src/components/UserPosts.jsx
@@ -1,22 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import SinglePost from "./SinglePost";
 import { useGetUserPostsQuery } from "../slices/usersApiSlice";
-import { Text, HStack, Heading } from "@chakra-ui/react";
+import { Text, HStack, Heading, Spinner, Button } from "@chakra-ui/react";
+import { useNavigate } from 'react-router-dom';
 
 const UserPosts = () => {
   const { data: posts, isLoading, error, refetch } = useGetUserPostsQuery();
+  const navigate = useNavigate();
   useEffect(() => {
     console.log("profile loaded")
     refetch()
   }, [])
+  const handleClick = () => {
+    navigate('/community');
+  }
   return (
     <div>
       <Heading size='xl' marginLeft={"10px"} color={"steelblue"}>Your Posts</Heading>
       <HStack align="start" spacing="4"></HStack>
       {isLoading ? (
-        <Text>Loading...</Text>
+        <Flex justifyContent="center" alignItems="center" height="80vh">
+          {" "}
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+          ></Spinner>
+        </Flex>
       ) : error ? (
         <Text color="red.500">{error?.data?.message || error.error}</Text>
+      ) : (posts.length === 0) ? (
+        <Button onClick={handleClick} colorScheme="teal" marginTop={"20px"}>Go to the community page create a post!</Button>
       ) : (
         <HStack align="start" spacing="4" wrap="wrap">
           {posts.map((post) => (
